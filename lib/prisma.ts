d51db7a96dec02089e5bc15d9c1aa115f8554ff6
kuku <kuku@kukus-Mac-mini.local>
2026-06-06 12:43:02 +0200

    fix: Railway Prisma build

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
-import { PrismaClient } from "@/app/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import path from "path";
 
 declare global {
